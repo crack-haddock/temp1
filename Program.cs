7e050f90d9024c65ae79e574655d8c6283e7d64b
crack-haddock <richardmoss@outlook.com>
2019-07-20 00:28:10 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("datetime is " + DateTime.Now);
+            //var c1 = new HelloWorld.MyClass();
+            Console.WriteLine($"Hello World! {new HelloWorld.MyClass().ReturnMessage()}");
+
+            var lines = ReadFrom("sampleQuotes.txt");
+            foreach(var line in lines) {
+                //Console.WriteLine(value: line);
+            }
+
+            Console.WriteLine();
+
+            foreach(var i in ComputeAges()) {
+                Console.WriteLine(i);
+            }
+        }
+
+    	static IEnumerable ComputeAges() {
+            yield return 2;
+            yield return 21;
+            
+            for(int i = 22; i < 32; i++) {
+                yield return i;
+            }
+        }
+
+        static IEnumerable<string> ReadFrom(string file)
+        {
+            string line;
+            using (var reader = File.OpenText(file))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+    }
+}
